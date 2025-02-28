f893507e7172904ff432b5abf91c14094a742e3a
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-02-27 19:17:50 -0500

    First commit reactGPT

---
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -0,0 +1,33 @@
+import { NavLink, Outlet } from "react-router-dom"
+import { menuRoutes } from "../router/router"
+import { SidebarMenuItem } from "../components/sidebar/SidebarMenuItem"
+
+export const DashboardLayout = () => {
+  return (
+    <main className="flex flex-row mt-7">
+      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-gray-800 bg-opacity-10 p-5 rounded-3xl">
+        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 bg-clip-text text-transparent">
+          ReactGPT<span className="text-indigo-500">.</span>
+        </h1>
+        <span className="text-xl bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 bg-clip-text text-transparent">
+          Bienvenido
+        </span>
+
+        <div className="border-gray-700 border my-3" />
+
+        {/* Opciones del menú */}
+        {menuRoutes.map((option) => (
+          <SidebarMenuItem key={option.to} {...option} />
+        ))}
+      </nav>
+
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)] bg-gray-800 bg-opacity-10 p-5 rounded-3xl text-transparent">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-col flex-auto h-full p-1 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 bg-clip-text text-transparent">
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
