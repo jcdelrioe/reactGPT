295893e50abaf92a5979e5e8366b9eecb0e09da2
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-02-28 19:39:14 -0500

    functional Orthographi page

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -1,6 +1,8 @@
 export * from "./chat-bubbles/GptMessage"
 export * from "./chat-bubbles/MyMessage"
 
+export * from "./chat-input-boxes/TextMessageBox"
+
 export * from "./loaders/Typingloader"
 
 export * from "./sidebar/SidebarMenuItem"
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,16 +1,57 @@
-import { GptMessage, MyMessage, Typingloader } from "../../components"
+import { useState } from "react"
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  Typingloader,
+} from "../../components"
+
+interface Message {
+  text: string
+  isGpt: Boolean
+}
 
 export const OrthographyPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true)
+    setMessages((prev) => [...prev, { text: text, isGpt: false }])
+
+    //TODO: UseCase
+
+    setIsLoading(false)
+
+    //TODO: Añadir el mensaje de isGpt en true
+  }
   return (
     <div className="chat-container">
       <div className="chat-messages">
         <div className="grid grid-cols-12 gap-y-2">
           {/* Bienvenida */}
           <GptMessage text="Hola JCDRE" />
-          <MyMessage text="Este es mi mensaje" />
-          <Typingloader className="fade-in" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text="Esto es de OpenAi" />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <Typingloader />
+            </div>
+          )}
         </div>
       </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Tu mensaje aqui"
+        disableCorrections
+      />
     </div>
   )
 }
