4893a48520cd4dcb550f059c72a4f41e817f580d
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-05-05 17:28:01 -0500

    pros const stream in frontend

--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,5 +1,78 @@
-import React from "react"
+import { useRef, useState } from "react"
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components"
+import { prosConsStreamGeneratorUseCase } from "../../../core/use-cases"
+
+interface Message {
+  text: string
+  isGpt: Boolean
+}
 
 export const ProsConsStreamPage = () => {
-  return <div>ProsConsStreamPage</div>
+  const abortController = useRef(new AbortController())
+  const isRunning = useRef(false)
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (text: string) => {
+    if (isRunning.current) {
+      abortController.current.abort()
+      abortController.current = new AbortController()
+    }
+    setIsLoading(true)
+    isRunning.current = true
+    setMessages((prev) => [...prev, { text: text, isGpt: false }])
+
+    //TODO: UseCase
+    const stream = prosConsStreamGeneratorUseCase(
+      text,
+      abortController.current.signal
+    )
+    setIsLoading(false)
+
+    setMessages((mesages) => [...mesages, { text: "", isGpt: true }])
+
+    for await (const text of stream) {
+      setMessages((messages) => {
+        const newMessages = [...messages]
+        newMessages[newMessages.length - 1].text = text
+        return newMessages
+      })
+    }
+    isRunning.current = false
+  }
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida */}
+          <GptMessage text="¿Que deseas comparar hoy?" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Tu mensaje aqui"
+        disableCorrections
+      />
+    </div>
+  )
 }
--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import { useState } from "react"
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components/index"
+import { prosConsUseCase } from "../../../core/use-cases"
+
+interface Message {
+  text: string
+  isGpt: Boolean
+}
 
 export const ProsConsPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true)
+    setMessages((prev) => [...prev, { text: text, isGpt: false }])
+
+    //TODO: UseCase
+    const { ok, content } = await prosConsUseCase(text)
+
+    setIsLoading(false)
+
+    if (!ok) return
+
+    //TODO: Añadir el mensaje de isGpt en true
+    setMessages((prev) => [...prev, { text: content, isGpt: true }])
+  }
   return (
-    <div>ProsConsPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida */}
+          <GptMessage text="Puedes escribir lo que quieras que compare y te de mis puntos de vista" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Tu mensaje aqui"
+        disableCorrections
+      />
+    </div>
   )
 }
